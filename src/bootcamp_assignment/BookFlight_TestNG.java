72404be997f9e10aa5a20bae0a27572ba2db79e5
kavyaka pellakuru <pellakurukavyaka@gmail.com>
2018-05-01 18:58:22 +0530

    trying merge

--- src/bootcamp_assignment/BookFlight_TestNG.java
+++ src/bootcamp_assignment/BookFlight_TestNG.java
@@ -9,5 +9,7 @@ public class BookFlight_TestNG extends WebActions {
 
 //I'm writing lines of code just to verify again 3 git commit changes	
   //as part project 2-coomit 1
+//trying merge
+//trying merge to master
 
 }
