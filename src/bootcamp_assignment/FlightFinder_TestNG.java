0dcac00c2a3cd0d3772246ce7ac0faced187479c
kavyaka pellakuru <pellakurukavyaka@gmail.com>
2018-05-01 16:52:56 +0530

    reverting 2 changes

--- src/bootcamp_assignment/FlightFinder_TestNG.java
+++ src/bootcamp_assignment/FlightFinder_TestNG.java
@@ -8,7 +8,12 @@ import org.testng.annotations.AfterMethod;
 public class FlightFinder_TestNG extends WebActions{
 	
 	
-		
+<<<<<<< HEAD
+	//comit 2
+//commit 3	
+=======
+	//writing something here	
+>>>>>>> parent of 4deae60... Revert "commit 1"
  
 
 }
